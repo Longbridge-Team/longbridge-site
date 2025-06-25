6505ea9eb8e9cb7be1a68a3147dd0dbe4c4a3ca3
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2025-06-25 20:49:42 +0300

    logo

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,7 @@ if (!isset($currentPage)) {
       <li><a <?php if($currentPage=='home') echo 'class="active"'; ?> href="/index.aspx"><img height="40" width="40" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.rw-designer.com%2Ficon-image%2F18837-256x256x4.png&f=1&nofb=1&ipt=f7d794f16772d7f4e0c9a6e94728373825d3c62652593016b99b5592e6f2ea32" alt="Home">Home</a></li>
       <li><a <?php if($currentPage=='features') echo 'class="active"'; ?> href="/features.aspx"><img height="40" width="40" src="https://images.freeimages.com/fic/images/icons/679/nx10/256/windows_update.png" alt="Features">Features</a></li>
       <li><a href="https://uploadnow.io/f/77m5F4p"><img height="40" width="40" src="http://www.jidesoft.com/icon/vista/vista.png" alt="Download">Download</a></li>
-      <li><a href="https://discord.gg/zP9fAPxtNj"><img height="40" width="40" src="https://upload.wikimedia.org/wikipedia/en/b/bf/Windows_Live_Messenger_icon.png" alt="Community">Community</a></li>
+      <li><a href="https://discord.gg/zP9fAPxtNj"><img height="40" width="40" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2Flne7g32kas2c1.png&f=1&nofb=1&ipt=1d67f20a42f2bc0901f6d1c4e7cedbe700edbbff8f4cf0c716d53327873b7d8c" alt="Community">Community</a></li>
       <li><a <?php if($currentPage=='about') echo 'class="active"'; ?> href="/about.aspx"><img height="40" width="40" src="https://www.iconshock.com/image/Vista/General/file" alt="About">About</a></li>
       <?php if(isset($_SESSION['user'])): ?>
       <li><a <?php if($currentPage=='chat') echo 'class="active"'; ?> href="/chat.aspx"><img height="40" width="40" src="https://upload.wikimedia.org/wikipedia/en/b/bf/Windows_Live_Messenger_icon.png" alt="Chat">Chat</a></li>
