26e82d8bbce92b3db87e10260d89705d760b7457
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2025-06-25 20:20:21 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,10 +4,12 @@
   <p>An independent reimagining of Longhorn on modern foundations.</p>
   <p>&copy; 2025 Windows Longbridge Project. Not affiliated with Microsoft.</p>
   <div class="footer-links">
-    <a href="https://github.com/Longbridge-Team">GitHub</a>
+    <img width="20" src="https://img.icons8.com/?size=80&amp;id=LoL4bFzqmAa0&amp;format=png" alt=""><a href="https://github.com/Longbridge-Team">GitHub</a>
     <span class="sep">|</span>
+    <img width="20" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2Flne7g32kas2c1.png&amp;f=1&amp;nofb=1&amp;ipt=1d67f20a42f2bc0901f6d1c4e7cedbe700edbbff8f4cf0c716d53327873b7d8c" alt="">
     <a href="https://discord.gg/zP9fAPxtNj">Discord</a>
     <span class="sep">|</span>
+    <img width="20" src="https://www.iconshock.com/image/Vista/General/book" alt="">
     <a href="/about.aspx">Docs</a>
   </div>
 </footer>
