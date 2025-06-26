5be082e55d0418cf9552e344d62a25d0fdca9d24
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2025-06-26 16:29:34 +0300

    error

--- 403.php
+++ 403.php
@@ -4,7 +4,7 @@ $title = '403 Forbidden - Windows Longbridge';
 include __DIR__ . '/includes/header.php';
 ?>
 <div class="panel">
-  <h2><img width="80" src="https://www.iconshock.com/image/RealVista/General/forbidden" alt="">Access Denied (403)</h2>
+  <h2><img width="80" src="https://www.iconshock.com//image/REALVISTA/development/logic_error" alt="">Access Denied (403)</h2>
   <p>You do not have permission to access this page.</p>
   <p><a href="/index.aspx">Return to the homepage</a></p>
 </div>
--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@ $title = '404 Not Found - Windows Longbridge';
 include __DIR__ . '/includes/header.php';
 ?>
 <div class="panel">
-  <h2><img width="80" src="https://www.iconshock.com/image/RealVista/General/error" alt="">Page Not Found (404)</h2>
+  <h2><img width="80" src="https://www.iconshock.com//image/REALVISTA/development/logic_error" alt="">Page Not Found (404)</h2>
   <p>The page you're looking for could not be found. It may have been moved or deleted.</p>
   <p><a href="/index.aspx">Return to the homepage</a></p>
 </div>
