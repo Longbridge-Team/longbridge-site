6a319f9e3788a2bf2bca2a4fa560ea209f02b05b
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2025-06-26 16:16:31 +0300

    Add custom 403 and 404 error pages

---
+++ 403.php
@@ -0,0 +1,11 @@
+<?php
+http_response_code(403);
+$title = '403 Forbidden - Windows Longbridge';
+include __DIR__ . '/includes/header.php';
+?>
+<div class="panel">
+  <h2><img width="80" src="https://www.iconshock.com/image/RealVista/General/forbidden" alt="">Access Denied (403)</h2>
+  <p>You do not have permission to access this page.</p>
+  <p><a href="/index.aspx">Return to the homepage</a></p>
+</div>
+<?php include __DIR__ . '/includes/footer.php'; ?>
